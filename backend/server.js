442e86dd66e77c8b20f6b1e2fb32c5a21b6b0ce9
wadpas <wadpas@gmail.com>
2024-09-19 23:13:40 +0300

    user back done

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 const port = process.env.PORT || 5000
@@ -9,11 +11,15 @@ dotenv.config()
 connectDB()
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
 	res.send('Hello World!')
 })
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
