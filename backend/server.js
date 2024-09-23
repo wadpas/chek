053ad1f964378783eb853b510f652408096556e6
wadpas <123688961+wadpas@users.noreply.github.com>
2024-09-23 12:15:47 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,7 @@ import productRoutes from './routes/productRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
-
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import { notFound, errorHandler } from '../backend/middleware/errorMIddleware.js'
 
 const port = process.env.PORT || 5000
 dotenv.config()
@@ -19,9 +18,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-app.get('/', (req, res) => {
-	res.send('Hello World!')
-})
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -31,8 +27,19 @@ app.get('/api/config/paypal', (req, res) =>
 		clientId: process.env.PAYPAL_CLIENT_ID,
 	})
 )
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+	const __dirname = path.resolve()
+	app.use('/uploads', express.static('/var/data/uploads'))
+	app.use(express.static(path.join(__dirname, '/frontend/build')))
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+	const __dirname = path.resolve()
+	app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+	app.get('/', (req, res) => {
+		res.send('API is running....')
+	})
+}
 
 app.use(notFound)
 app.use(errorHandler)
